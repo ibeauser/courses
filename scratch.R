41a57686c575306ea6cdd019cb31f45d72ee1288
ibeauser <ibeauser@gmail.com>
2015-04-06 11:23:46 -0400

    update repo

---
+++ scratch.R
@@ -0,0 +1,6 @@
+.5^4 * (1 - .5)^1 + .5^5 * (1 - .5)^0
+choose(5,4) * .5^5 + choose(5,5) * .5^5
+ppois(10, lambda = 5 * 3)
+pbinom(4, size = 5, prob = 0.5, lower.tail = FALSE)
+choose(6,5) * .5^6 + choose(6,6) * .5^6
+electoral_districts <- raster("Users/jonathan/Downloads/2014_GE_Polling_Divisions/2014_GE_Polling_division.shp")
